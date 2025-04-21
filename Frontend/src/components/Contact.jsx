c244f99eb75fd2806314b5a16928ad03f8902586
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-21 21:49:23 +0530

    host backend and replace frontend url

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
     setLoading(true);
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/send/mail",
+        "https://gym-mern-r7c8.onrender.com/send/mail",
         {
           name,
           email,
