0799e032acca7b9c465db49ab02385b70f51eced
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-21 22:01:06 +0530

    add frontend url in backend

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ config({ path: "./config.env" });
 
 app.use(
     cors({
-        origin: [process.env.FRONTEND_URL],
+        origin: 'https://gym-mern-1.onrender.com/',
         methods: ["POST"],
         credentials: true,
     })
