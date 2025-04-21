86ba2e8587fabec715dd62509e63038cd03218da
Ashish Maddhesiya <ashishmadeshiya17@gmail.com>
2025-04-21 22:33:31 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ config({ path: "./config.env" });
 
 app.use(
     cors({
-        origin: "*",
+        origin: process.env.FRONTEND_URL,
         methods: ["POST"],
         credentials: true,
     })
